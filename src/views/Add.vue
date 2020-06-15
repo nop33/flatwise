4eb02af511691d860ac58b2f079f2ed4366d9d75
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-06-15 21:33:38 +0200

    Add list of items and add new item views
    as well as vuetify

---
+++ src/views/Add.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h1>Add a new item</h1>
+    <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            label="Item name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="price"
+            :rules="priceRules"
+            label="Purchace price"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-dialog
+            ref="dialog"
+            :return-value.sync="date"
+            persistent
+            width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                label="Purchace date"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="date" scrollable @input="$refs.dialog.save(date)"></v-date-picker>
+          </v-dialog>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-btn
+          color="warning"
+          @click="save"
+        >
+          Save
+        </v-btn>
+      </v-row>
+    </v-container>
+  </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      valid: false,
+      name: '',
+      price: 0,
+      date: new Date().toISOString().substr(0, 10),
+      nameRules: [
+        v => !!v || 'Name is required'
+      ],
+      priceRules: [
+        v => !!v || 'Price is required',
+        v => v > 0 || 'Price cannot be 0 or below'
+      ]
+    }
+  },
+  methods: {
+    save () {
+      alert('Saved! NOT!')
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <v-container>
+    <v-row justify="center">
+      <v-col md="8">
+        <v-data-table
+          :headers="headers"
+          :items="items"
+          :items-per-page="20"
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      depreciationValue: 0.2,
+      headers: [
+        {
+          text: 'Item',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Price', value: 'price' },
+        { text: 'Purchace Date', value: 'date' },
+        { text: 'Number of days since purchace', value: 'noDaysBetween' }
+      ],
+      items: [
+        {
+          name: 'Sofa',
+          price: 1250,
+          date: '2019-08-01',
+          noDaysBetween: 0
+        },
+        {
+          name: 'Balcony rug',
+          price: 70,
+          date: '2020-06-01',
+          noDaysBetween: 0
+        }
+      ]
+    }
+  },
+  created () {
+    this.items.forEach(element => {
+      element.noDaysBetween = numberOfDaysOwned(element.date)
+    })
   }
+}
+
+function numberOfDaysOwned (date) {
+  return Math.floor(daysBetween(date, new Date()))
+}
+
+function treatAsUTC (date) {
+  var result = new Date(date)
+  result.setMinutes(result.getMinutes() - result.getTimezoneOffset())
+  return result
+}
+
+function daysBetween (startDate, endDate) {
+  var millisecondsPerDay = 24 * 60 * 60 * 1000
+  return (treatAsUTC(endDate) - treatAsUTC(startDate)) / millisecondsPerDay
 }
 </script>
