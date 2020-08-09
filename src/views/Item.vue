e6efae326e5c45d5af8e00652ca9048685240f1b
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-08-09 15:39:34 +0200

    Add more item details in item view

--- src/views/Item.vue
+++ src/views/Item.vue
@@ -1,18 +1,32 @@
 <template>
   <div>
-    <v-toolbar flat color="primary" dark>
+    <v-app-bar app flat color="primary" dark prominent hide-on-scroll >
       <v-btn icon @click="goToFlat">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-      <v-toolbar-title>{{ item.name }}</v-toolbar-title>
-      <template v-slot:extension>
-        <v-btn color="red" dark absolute bottom right fab @click="edit">
-          <v-icon>mdi-pencil-outline</v-icon>
-        </v-btn>
-      </template>
-    </v-toolbar>
+      <v-toolbar-title>
+        <div class="">{{ item.name }}</div>
+        <div class="mt-2">{{ item.price }} CHF</div>
+        <div class="text-caption mt-1">Bought on {{ item.date }}</div>
+      </v-toolbar-title>
+      <v-btn color="red" dark absolute bottom right fab @click="edit">
+        <v-icon>mdi-pencil-outline</v-icon>
+      </v-btn>
+    </v-app-bar>
     <v-main>
-      <!-- <Item :item="item" /> -->
+      <v-list>
+        <v-subheader>Shared amongst</v-subheader>
+        <v-list-item v-for="flatmate in item.shareAmongst" :key="flatmate">
+          <v-list-item-avatar>
+            <v-avatar color="primary">
+              <span class="white--text">XY</span>
+            </v-avatar>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ flatmate }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-main>
   </div>
 </template>
