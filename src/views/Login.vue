7a2da157d2ee06458049612a877d585e493442e4
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-08-13 14:27:07 +0200

    Improve style of sign in page and add Email

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,13 @@
 <template>
-  <section id="firebaseui-auth-container"></section>
+  <v-container class="fill-height sign-in" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <div class="text-center text-h1">🏠</div>
+        <div class="text-center text-h3">Flatwise</div>
+        <section id="firebaseui-auth-container"></section>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -13,9 +21,18 @@ export default {
     var ui = new firebaseui.auth.AuthUI(firebase.auth())
     var uiConfig = {
       signInSuccessUrl: '/register',
-      signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ]
     }
     ui.start('#firebaseui-auth-container', uiConfig)
   }
 }
 </script>
+
+<style lang="scss">
+.sign-in .firebaseui-idp-list {
+  padding-left: 0;
+}
+</style>
