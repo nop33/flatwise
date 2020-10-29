83cd89fd402abcf7764831b9346da94a3bf5ce3c
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-10-29 12:45:33 +0100

    Generate proper initials for avatar

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-avatar :color="color" :size="size" :left="left">
     <img v-if="user.photo" :src="user.photo" alt="Avatar"/>
-    <span v-else-if="user.name" class="white--text">{{ user.name.substring(0, 2).toUpperCase() }}</span>
+    <span v-else-if="user.name" class="white--text">{{ initials }}</span>
   </v-avatar>
 </template>
 
@@ -17,6 +17,11 @@ export default {
   computed: {
     color () {
       return stringToHslColor(this.user.name)
+    },
+    initials () {
+      const words = this.user.name.split(' ')
+      const initials = words.length > 1 ? `${words[0].charAt(0)}${words[1].charAt(0)}` : `${words[0].charAt(0)}${words[0].charAt(1)}`
+      return initials.toUpperCase()
     }
   }
 }
