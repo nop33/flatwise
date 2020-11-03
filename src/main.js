71334a7cda75192493cf6f03cd85ed790e74e3eb
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-11-03 21:57:32 +0100

    Move firestore initialization in separate file

--- src/main.js
+++ src/main.js
@@ -1,33 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
 import VueFirestore from 'vue-firestore'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import firestore from './firebase.js'
 import { createUserObject } from './store/models.js'
 
 Vue.config.productionTip = false
 
-require('dotenv').config()
-
-require('firebase/firestore')
-
 Vue.use(VueFirestore)
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID
-})
-
-const firestore = firebaseApp.firestore()
-
 Vue.prototype.$db = {
-  users: firestore.collection('users'),
-  flats: firestore.collection('flats')
+  users: firestore.collection('users')
 }
 
 Vue.filter('round', function (value) {
