14c7690b0b40ce6da3226747ec04c141a4585677
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-11-12 14:58:02 +0100

    Fix flat edit form validation

--- src/views/FlatEdit.vue
+++ src/views/FlatEdit.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
-    <v-toolbar flat color="primary" dark fixed>
-      <v-btn icon @click="$router.go(-1)">
-        <v-icon>mdi-arrow-left</v-icon>
-      </v-btn>
-      <v-toolbar-title>Edit flat settings</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text large @click="save">Save</v-btn>
-    </v-toolbar>
+    <AppBarThin
+      :backButtonCallback="goToFlat"
+      title="Edit flat settings"
+      :actionButtonCallback="save"
+      actionButtonText="Save"
+    />
     <v-main>
     <FlatForm v-model="flat" edit />
     <FlatmateGroupList :flatmates="currentFlatmates(flatId)" :flatId="flatId" />
@@ -28,11 +26,13 @@ import { getFlatFromStateById } from '@/utils/getters'
 
 import FlatForm from '@/components/FlatForm.vue'
 import FlatmateGroupList from '@/components/FlatmateGroupList.vue'
+import AppBarThin from '@/components/AppBarThin.vue'
 
 export default {
   components: {
     FlatForm,
-    FlatmateGroupList
+    FlatmateGroupList,
+    AppBarThin
   },
   props: [
     'flatId'
@@ -51,10 +51,11 @@ export default {
     this.flat = getFlatFromStateById(this.flatId)
   },
   methods: {
+    goToFlat () {
+      this.$router.push({ name: 'Flat', params: { flatId: this.flatId } })
+    },
     save () {
-      this.$store.dispatch('updateFlat', this.flat).then(() => {
-        this.$router.push({ name: 'Flat', params: { flatId: this.flatId } })
-      })
+      this.$store.dispatch('updateFlat', this.flat).then(this.goToFlat)
     },
     deleteFlat () {
       // TODO
