1129d9c59dad08ad458e64e49fa68f50ac3ee6f4
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-11-15 17:02:05 +0100

    Testing child components

---
+++ src/components/ItemList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Item v-for="item in displayItems" :item="item" :key="item.id" />
+  </div>
+</template>
+
+<script>
+import Item from '@/components/Item.vue'
+
+export default {
+  components: {
+    Item
+  },
+  data: () => {
+    return {
+      displayItems: window.items
+    }
+  }
+}
+</script>
