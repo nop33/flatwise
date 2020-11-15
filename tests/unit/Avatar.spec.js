be8b0fdeeb6c348f98c0f841e2a96558ac1c48d9
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-11-15 15:55:11 +0100

    Create first component test

--- tests/unit/Avatar.spec.js
+++ tests/unit/Avatar.spec.js
@@ -1,1 +1,10 @@
-test('sanity text', () => {})
+import Item from '@/components/Item.vue'
+import Vue from 'vue'
+
+describe('Item.vue', () => {
+  test('sanity text', () => {
+    const Ctor = Vue.extend(Item)
+    const vm = new Ctor().$mount()
+    expect(vm.$el.textContent).toContain('item text')
+  })
+})
