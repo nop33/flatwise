d26774cafe1f433b5199dff4381c08ddcca864cf
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-11-20 13:04:06 +0100

    Move around some mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,12 +1,37 @@
 export default {
-  SET_CURRENT_FLAT_ID (state, id) {
-    state.currentFlatId = id
+  SET_USER (state, user) {
+    state.user = user
   },
   SET_PAGE_FORM_VALIDITY (state, validity) {
     state.isPageFormValid = validity
   },
-  SET_USER (state, user) {
-    state.user = user
+  TOGGLE_LOADER (state, toggle) {
+    state.loading = toggle
+  },
+  SET_CURRENT_FLAT_ID (state, id) {
+    state.currentFlatId = id
+  },
+  CREATE_FLAT (state, flatData) {
+    state.flats.push(flatData)
+  },
+  SET_FLAT_ITEMS (state, { flat, items }) {
+    flat.items = items
+  },
+  SET_FLATS (state, flats) {
+    state.flats = flats
+  },
+  UPDATE_FLAT (state, { flat, flatData }) {
+    flat.name = flatData.name
+    flat.depreciationRate = flatData.depreciationRate
+    flat.lowestPriceRate = flatData.lowestPriceRate
+  },
+  UPDATE_FLAT_ITEMS_SHARING_INFO (state, { flat, flatmateId, itemIdsToBeRemovedFrom, itemIdsToBeAddedAt }) {
+    flat.items.filter(item => itemIdsToBeRemovedFrom.includes(item.id)).forEach(item => {
+      item.idsOfFlatmatesThatShareThis.splice(item.idsOfFlatmatesThatShareThis.indexOf(flatmateId), 1)
+    })
+    flat.items.filter(item => itemIdsToBeAddedAt.includes(item.id)).forEach(item => {
+      item.idsOfFlatmatesThatShareThis.push(flatmateId)
+    })
   },
   ADD_ITEM (state, { itemData, flat }) {
     flat.items.push(itemData)
@@ -23,25 +48,6 @@ export default {
   DELETE_ITEM (state, { itemData, flat }) {
     flat.items.splice(flat.items.indexOf(flat.items.find(item => item.id === itemData.id)), 1)
   },
-  TOGGLE_LOADER (state, toggle) {
-    state.loading = toggle
-  },
-  CREATE_FLAT (state, flatData) {
-    state.flats.push(flatData)
-  },
-  UPDATE_FLAT (state, flatData) {
-    const flat = state.flats.find(flat => flat.id === flatData.id)
-    flat.name = flatData.name
-    flat.depreciationRate = flatData.depreciationRate
-    flat.lowestPriceRate = flatData.lowestPriceRate
-  },
-  SET_FLATS (state, flats) {
-    state.flats = flats
-    state.isStoreInitialized = true
-  },
-  SET_FLAT_ITEMS (state, { flat, items }) {
-    flat.items = items
-  },
   ADD_FLATMATE (state, { flat, flatmateData }) {
     flat.flatmates.push(flatmateData)
   },
@@ -54,13 +60,5 @@ export default {
   SET_FLATMATE_MOVE_OUT_DATE (state, { flat, flatmateId, moveOutDate }) {
     const flatmate = flat.flatmates.find(flatmate => flatmate.id === flatmateId)
     flatmate.endDate = moveOutDate
-  },
-  UPDATE_FLAT_ITEMS_SHARING_INFO (state, { flat, flatmateId, itemIdsToBeRemovedFrom, itemIdsToBeAddedAt }) {
-    flat.items.filter(item => itemIdsToBeRemovedFrom.includes(item.id)).forEach(item => {
-      item.idsOfFlatmatesThatShareThis.splice(item.idsOfFlatmatesThatShareThis.indexOf(flatmateId), 1)
-    })
-    flat.items.filter(item => itemIdsToBeAddedAt.includes(item.id)).forEach(item => {
-      item.idsOfFlatmatesThatShareThis.push(flatmateId)
-    })
   }
 }
